9430962849cd414d5d3da727aea9685010626bdf
Luka <nickolas.kanno@gmail.com>
2022-05-23 14:36:15 -0700

    9.3

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
+const inquirer = require('inquirer');
+const promptUser = () => {
+return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ]);
+};
+  promptUser().then(answers => console.log(answers));
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
+
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw err;
+
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
