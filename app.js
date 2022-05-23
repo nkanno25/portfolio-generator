75722186fe651a3df7f1b1f6eaa5f3ecbfed49a2
Luka <nickolas.kanno@gmail.com>
2022-05-23 15:46:27 -0700

    9.3.5

--- app.js
+++ app.js
@@ -1,14 +1,75 @@
 const inquirer = require('inquirer');
+
 const promptUser = () => {
-return inquirer.prompt([
-    {
-      type: 'input',
-      name: 'name',
-      message: 'What is your name?'
-    }
-  ]);
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username'
+      },
+      {
+        type: 'input',
+        name: 'about',
+        message: 'Provide some information about yourself:'
+      }
+    ]);
+  };
+
+  const promptProject = portfolioData => {
+      // if theres no projects array property, create one
+      if (!portfolioData.projects) {
+    portfolioData.projects = [];
+      }
+
+    console.log(`
+    // add a new project
+    `);
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is the name of your project"
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Provide a description of the project (required)"
+        },
+        {
+            type: "checkbox",
+            name: "languages",
+            message: "What did you build this project with? (Check all that apply)",
+            choices: ["JavaScript", "HTML", "CSS", "ES6", "jQuery", "Bootstrap", "Node"]
+        },
+        {
+            type: "input",
+            name: "link",
+            message: "Enter the Github link to your project. (Required)"
+        },
+        {
+            type: "confirm",
+            name: "feature",
+            message: "Would you like to feature this project?",
+            default: false
+        },
+        {
+            type: "confirm",
+            name: "confirmAddProject",
+            message: "Would you like to enter another project",
+            default: false
+        }
+    ]);
 };
-  promptUser().then(answers => console.log(answers));
+promptUser()
+  .then(promptProject)
+  .then(portfolioData => {
+    console.log(portfolioData);
+  });
 // const fs = require('fs');
 // const generatePage = require('./src/page-template');
 
